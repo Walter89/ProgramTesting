d6066aa5c3590f5488d58f9a0d7414f7ffd653f6
Magnus <Magnus@Magnus-PC>
2013-02-05 20:29:45 +0100

    hej

--- testlabb1/src/TriangleTest.java
+++ testlabb1/src/TriangleTest.java
@@ -1,100 +1,315 @@
-
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-
-
 /**
- * TriangleTest for testing the Triangle class.
- * This template is used in the exercise phase 1.
- * Students should add relevant unit test cases related to the Triangle 
- * class to this class.
+ * TriangleTest for testing the Triangle class. This template is used in the
+ * exercise phase 1. Students should add relevant unit test cases related to the
+ * Triangle class to this class.
  */
-public class TriangleTest extends TestCase
-{
-	private Triangle rightAngledTriangle;
-	private Triangle imposTriangle;
+public class TriangleTest extends TestCase {
+
+	private Triangle EC1;
+	private Triangle EC2;
+	private Triangle EC3;
+	private Triangle EC4;
+	private Triangle EC8;
+	private Triangle EC9;
+	private Triangle EC16;
+	private Triangle EC19;
+	private Triangle EC40;
+	private Triangle EC37;
+	private Triangle EC39;
+	private Triangle almostRightAngle;
+	private Triangle almostIsossceles;
+	private Triangle almostEquilateral;
+	private Triangle almostEquilateral2;
+	private Triangle EC10;
+	private Triangle EC5;
+	private Triangle EC6;
+	private Triangle EC7;
+
 	/**
 	 * Constructs a test case with the given name.
 	 */
-	public TriangleTest(String name)
-	{
+	public TriangleTest(String name) {
 		super(name);
 	}
 
-	/** 
-	 * Initializes common objects. The JUnit framework automatically invokes 
+	/**
+	 * Initializes common objects. The JUnit framework automatically invokes
 	 * this method before each test is run.
 	 */
-	protected void setUp()
-	{
-		rightAngledTriangle = new Triangle(3, 4, 5);
+	protected void setUp() {
+		EC1 = new Triangle(1, 1, 1);//BVA/EP, valid
+		EC2 = new Triangle(0, 0, 0);//BVAEP, inte valid
+		EC3=new Triangle(0,0,1);//EP invalid
+		EC5=new Triangle(0,1,1);//EP invalid
+		EC6=new Triangle(1,1,-1);//EP invalid
+		EC7=new Triangle(1,-1,-1);//EP invalid
+		EC4 = new Triangle(2, 4, 5);//valid, BVA 
+		EC8 = new Triangle(2, 2, 5);//invalid,BVA
+		EC9 = new Triangle(2, 2, 2);//valid EP
+		//EC10 = new Triangle(2, "k", 2);//invalid EP
+		EC16 = new Triangle(5, 4, 5);//likbent EP
+		EC19 = new Triangle(5, 4, 3);//rätvinkel, BVA
+		EC40 = new Triangle(Integer.MAX_VALUE, Integer.MAX_VALUE,
+				Integer.MAX_VALUE);//BVA stort.
+		EC37 = new Triangle(1, 2, 3);//scalene BVA
+		EC39 = new Triangle(3, 4, 5);//rätvinkel, BVA
+		almostRightAngle = new Triangle(270, 110, 250);//BVA, nästan rät
+		almostIsossceles = new Triangle(300, 301, 100);//BVA, nästan likbent
+		almostEquilateral = new Triangle(300, 300, 301);//BVA, nästan liksidig, övre
+		almostEquilateral2 = new Triangle(300, 300, 299);//BVA, nästan liksidig, undre
 	}
 
 	/**
-	 * Cleanup method. The JUnit framework automatically invokes
-	 * this method after each test is run.
+	 * Cleanup method. The JUnit framework automatically invokes this method
+	 * after each test is run.
 	 */
-	protected void tearDown()
-	{
-		rightAngledTriangle = null;
+	protected void tearDown() {
+		EC1 = null;
+		EC2 = null;
+		EC3 = null;
+		EC4 = null;
+		EC8 = null;
+		EC9 = null;
+		EC16 = null;
+		EC19 = null;
+		EC40 = null;
+		EC37 = null;
+		EC39 = null;
+		almostRightAngle = null;
+		almostIsossceles = null;
+		almostEquilateral = null;
+		almostEquilateral2 = null;
+
 	}
-	
+
 	/**
-	 * Test whether the triangle specified in the fixture (setUp) 
-	 * is right-angled. 	
+	 * Test whether the triangle specified in the fixture (setUp) is
+	 * right-angled.
 	 */
-	public void testRightAngled()
-	{
+	public void testRightAngledBVAEP() {
 		assertTrue("Should return true for a right-angled triangle",
-		            rightAngledTriangle.isRightAngled());
-		assertEquals("Should return 'right-angled'", "right-angled", rightAngledTriangle.classify());			
+				EC19.isRightAngled()); // borde returnera true, fast var false
 	}
+
+	public void testRightAngled2BVAEP() {
+		assertTrue("Should return true for a right-angled triangle",
+				EC39.isRightAngled());// funkar
+	}
+
+	public void testRightAngled3BVAEP() {
+		assertEquals("Should return 'right-angled'", "right-angled",
+				EC19.classify()); // borde ge right angle, inte scalene
+	}
+
+	public void testRightAngled4EP() {
+		assertFalse("Should return false for a right-angled triangle",
+				EC9.isRightAngled());// funkar
+	}
+
+	public void testRightAngled5BVA() {
+		assertFalse("Should return false for a right-angled triangle",
+				almostRightAngle.isRightAngled());// funkar inte
+	}
+
+	public void testIsImpossibleBVAEP() {
+		assertFalse("Should return false", EC1.isImpossible());
+
+	}
+
+	public void testIsImpossible2BVA() {
+		assertTrue("Should return true", EC2.isImpossible());
+	}
+
+	public void testIsImpossible3BVA() {
+		assertFalse("Should return false", EC4.isImpossible());
+	}
+
+	public void testIsImpossible4BVA() {
+		assertTrue("Should return true", EC8.isImpossible()); // ska avra
+																// omöjlig
+	}
+
+	public void testIsImpossible5EP() {
+		assertFalse("Should return false", EC9.isImpossible());
+
+	}
+	/**
+	public void testIsImpossible6EP() {
+		assertTrue("Should return true", EC10.isImpossible());
+	}
+*/
+	public void testIsImpossible7EP() {
+		assertTrue("Should return true", EC3.isImpossible());
+	}
+	public void testIsImpossible8EP() {
+		assertTrue("Should return true", EC5.isImpossible());
+	}
+	public void testIsImpossible9EP() {
+		assertTrue("Should return true", EC6.isImpossible());
+	}
+	public void testIsImpossible10EP() {
+		assertTrue("Should return true", EC7.isImpossible());
+	}
+	public void testGetSideLengthBVAEP() {
+		assertEquals("Should return '2,4,5'", "2,4,5", EC4.getSideLengths());
+
+	}
+
+	public void testclassifyBVAEP() {
+
+		assertEquals("Should return 'equilateral'", "equilateral",
+				EC1.classify());
+	}
+
+	public void testclassify2EP() {
+		assertFalse("Should not return 'equilateral'",
+				EC16.classify().equals("equilateral"));
+	}
+
+	public void testclassify3EP() {
+		assertEquals("Should return 'isossceles'", "isossceles",
+				EC16.classify()); // borde ge likbent, fast var Scale
+	}
+
+	public void testclassify4BVAEP() {
+		assertFalse("Should not return 'isossceles'",
+				EC1.classify().equals("isossceles"));
+	}
+
 	
 
+	public void testclassify6BVAEP() {
+		assertFalse("Should not return 'right-angled'",
+				EC1.classify().equals("right-angled"));
+	}
 
-	 public void testIsImpossible()
-		{
-		 assertTrue("Should return true",
-					imposTriangle.isImpossible());
-			assertEquals("Should return 'right-angled'", "right-angled", rightAngledTriangle.classify());					
-			imposTriangle=new Triangle(-1,-1,-1);
-			assertTrue("Should return true",
-					imposTriangle.isImpossible());
-			assertEquals("Should return 'right-angled'", "right-angled", rightAngledTriangle.classify());					
-		
-			imposTriangle=new Triangle(2,4,5);
-			assertTrue("Should return false",
-					!imposTriangle.isImpossible());
-			assertEquals("Should return 'right-angled'", "right-angled", rightAngledTriangle.classify());					
-		
-			imposTriangle=new Triangle(2,2,5);
-			assertTrue("Should return true",
-					imposTriangle.isImpossible());
-			assertEquals("Should return 'right-angled'", "right-angled", rightAngledTriangle.classify());					
-		
-		
-		}
+	public void testclassify7BVAEP() {
+		assertEquals("Should return 'scalene'", "scalene", EC37.classify());
+	}
+
+	public void testclassify8BVAEP() {
+		assertFalse("Should not return 'scalene'",
+				EC1.classify().equals("scalene"));
+
+	}
+
+	public void testGetAreaBVAEP() {
+		assertEquals("Should return '0.4330127018922193'", 0.4330127018922193,
+				EC1.getArea());
+	}
+
+	public void testGetArea2EP() {
+		assertEquals("Should return '9.16515138991168'", 9.16515138991168,
+				EC16.getArea());
+	}
+
+	public void testGetArea3EPBVA() {
+		assertEquals("Should return '6.0'", 6.0, EC19.getArea());
+	}
+
+	public void testGetArea4BVAEP() {
+		assertEquals("Should return '3.799671038392666'", 3.799671038392666,
+				EC4.getArea());// ska inte avra 0
+	}
+
+	public void testGetPerimeterBVA() {
+		assertEquals("Should return '3*Integer.MAX_VALUE",
+				3 * Integer.MAX_VALUE, EC40.getPerimeter());
+	}
+
+	public void testGetPerimeter2BVAEP() {
+		assertEquals("Should return '3'", 3, EC1.getPerimeter());
+	}
+
+	public void testGetPerimeter3BVA() {
+		assertEquals("Should return '0'", 0, EC2.getPerimeter());// borde
+																	// resultera
+																	// i
+																	// invalid(exception)
+
+	}
+
+	public void testisIsoscelesBVAEP() {
+		assertTrue("Should  return 'TRUE'", EC16.isIsosceles());
+	}
+
+	public void testisIsosceles2BVAEP() {
+		assertTrue("Should  return 'TRUE'", EC1.isIsosceles());
+	}
+
+	public void testisIsosceles3BVAEP() {
+		assertFalse("Should  return 'FALSE'", EC19.isIsosceles());
+	}
+
+	public void testisIsosceles4BVA() {
+		assertFalse("Should  return 'FALSE'", almostIsossceles.isIsosceles());
+	}
+
+	public void testisEquilateralBVAEP() {
+		assertTrue("Should  return 'TRUE'", EC1.isEquilateral());
+	}
+
+	public void testisEquilateral2BVAEP() {
+		assertFalse("Should  return 'FALSE'", EC16.isEquilateral());// Den är
+																	// likbent,
+																	// men inte
+																	// liksidig.
+	}
+
+	public void testisEquilateral3BVA() {
+		assertFalse("Should  return 'FALSE'", almostEquilateral.isEquilateral());
+	}
+
+	public void testisEquilatera4l() {
+		assertFalse("Should  return 'FALSE'",
+				almostEquilateral2.isEquilateral());
+	}
+
+	public void testIsScaleneBVAEP() {
+		assertTrue("Should  return 'True'", EC16.isIsosceles()); 
+	}
+
+	public void testIsScalene2BVAEP() {
+
+		assertFalse("Should  return 'False'", EC1.isScalene());
+	}
+
+	public void testIsScalene3BVA() {
+		assertFalse("Should  return 'FALSE'", EC2.isScalene());
+	}
+
+	public void testIsScalene4BVAEP() {
+		assertTrue("Should  return 'TRUE'", EC37.isScalene());
+	}
+
+	public void testIsScalene5EP() {
+		assertFalse("Should  return 'FALSE'", EC9.isScalene());
+	}
+
 	/**
-	 * Creates a test suite. You can use this if you have a hierarchy of 
-	 * suites where a suite higher in the hierarchy can have 
+	 * Creates a test suite. You can use this if you have a hierarchy of suites
+	 * where a suite higher in the hierarchy can have
 	 * suite.addTest(TriangleTest.suite())
-	 * @return a test suite 
+	 * 
+	 * @return a test suite
 	 */
-	public static Test suite()
-	{
+	public static Test suite() {
 		TestSuite suite = new TestSuite(TriangleTest.class);
 		return suite;
 	}
 
 	/**
 	 * Main function for starting the TestRunner.
-	 * @param args no parameters required.
+	 * 
+	 * @param args
+	 *            no parameters required.
 	 */
-	public static void main(String args[])
-	{
-		String[] testCaseName = { TriangleTest.class.getName()};
+	public static void main(String args[]) {
+		String[] testCaseName = { TriangleTest.class.getName() };
 
 		// chooses the userinterface
 
@@ -103,3 +318,4 @@ public class TriangleTest extends TestCase
 		junit.swingui.TestRunner.main(testCaseName);
 	}
 }
+
